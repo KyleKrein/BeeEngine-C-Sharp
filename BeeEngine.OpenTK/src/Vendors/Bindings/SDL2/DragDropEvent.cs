008968fbf064c473ddacf60d6b9cdbbc1de52df1
Александр Лебедев <alex.lebedev2003@icloud.com>
2023-01-10 00:14:47 +0300

    Added some bindings from https://github.com/mellinoe/veldrid/ Configured Nuget package creation

---
+++ BeeEngine.OpenTK/src/Vendors/Bindings/SDL2/DragDropEvent.cs
@@ -0,0 +1,12 @@
+namespace SDL2
+{
+    public struct DragDropEvent
+    {
+        public string File { get; }
+
+        public DragDropEvent(string file)
+        {
+            File = file;
+        }
+    }
+}
